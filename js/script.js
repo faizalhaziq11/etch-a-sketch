23a55d9d2ccde76517f1ddbe71885b5b5ce4be69
faizalhaziq11 <faizalhaziq1189@gmail.com>
2023-03-29 12:45:27 +0800

    Fix the color selector

--- js/script.js
+++ js/script.js
@@ -1,35 +1,60 @@
-// console.log('hello world')
-
 const defaultColor = `#ffffff`;
-const defaultSize = `53`
-
-function eraser() {
-  console.log('delete')
-}
-
-function clean() {
-  // output.innerHTML = `<span>${defaultSize} x ${defaultSize}</span>`
-  // showColor.innerHTML = `<span>${defaultColor}</span>`;
-  document.getElementById("colorpicker").value = defaultColor
-  document.getElementById("myRange").value = defaultSize
-
-}
-
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = `<span class="">${slider.value} x ${slider.value}</span>`; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function () {
-  output.innerHTML = `<span>${this.value} x ${this.value}</span>`
-}
+const defaultSize = `16`;
+let currentColor = `#780780`;
 
 var color = document.getElementById("colorpicker");
 var showColor = document.getElementById("showcolor");
-showColor.innerHTML = `<span>${color.value}</span>`; // Display the default color value
+showColor.innerHTML = `<span>${color.value}</span>`;
 
-// Update the current color value (each time you pick a new color)
 color.oninput = function () {
   showColor.innerHTML = `<span>${this.value}</span>`;
+};
 
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = `<span class="">${slider.value} x ${slider.value}</span>`;
+
+slider.oninput = function () {
+  output.innerHTML = `<span>${this.value} x ${this.value}</span>`;
+  grid.innerHTML = "";
+  makeRows(this.value, this.value);
+};
+
+function eraser() {
+  console.log("delete");
+}
+
+function clean() {
+  document.getElementById("colorpicker").value = defaultColor;
+  document.getElementById("myRange").value = defaultSize;
+  grid.innerHTML = "";
+  makeRows(+defaultSize, +defaultSize);
+  setColor(defaultColor);
+  output.innerHTML = `<span>${defaultSize} x ${defaultSize}</span>`;
+}
+
+var grid = document.getElementById("grid");
+
+function makeRows(rows, cols) {
+  grid.style.setProperty("--grid-rows", rows);
+  grid.style.setProperty("--grid-cols", cols);
+
+  for (c = 0; c < rows * cols; c++) {
+    let cell = document.createElement("div");
+    cell.addEventListener("mouseover", () => {
+      cell.style.backgroundColor = currentColor;
+    });
+    grid.appendChild(cell).className = "grid-item";
+  }
+}
+makeRows(+defaultSize, +defaultSize);
+
+let colorPicker = document.getElementById("colorpicker");
+
+colorPicker.addEventListener("input", () => {
+  setColor(colorPicker.value);
+});
+
+function setColor(color) {
+  return (currentColor = color);
 }
